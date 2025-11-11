563e4a8c1b3e413d7ee39d42f32dd445788457ad
Delmat237 <azangueleonel9@gmail.com>
2025-11-11 10:03:02 +0100

    merge wits stuff

--- tp4-ecommerce/app/Http/Controllers/DiscountCodesController.php
+++ tp4-ecommerce/app/Http/Controllers/DiscountCodesController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
+use App\Models\DiscountCodes;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class DiscountCodesController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -34,7 +34,7 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show(DiscountCodes $discountCodes)
     {
         //
     }
@@ -42,7 +42,7 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    public function edit(DiscountCodes $discountCodes)
     {
         //
     }
@@ -50,7 +50,7 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, DiscountCodes $discountCodes)
     {
         //
     }
@@ -58,7 +58,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy(DiscountCodes $discountCodes)
     {
         //
     }

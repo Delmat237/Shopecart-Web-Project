d98da5f7d4c0ee996bdc3c0939891cbbf631363e
Delmat237 <azangueleonel9@gmail.com>
2025-11-12 11:29:10 +0100

    auhtentification ok

--- tp4-ecommerce/app/Models/User.php
+++ tp4-ecommerce/app/Models/User.php
@@ -8,6 +8,47 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @OA\Schema(
+ * schema="User",
+ * title="User",
+ * description="Modèle de données pour un utilisateur",
+ * @OA\Property(
+ * property="id",
+ * type="integer",
+ * description="ID de l'utilisateur"
+ * ),
+ * @OA\Property(
+ * property="name",
+ * type="string",
+ * description="Nom complet de l'utilisateur"
+ * ),
+ * @OA\Property(
+ * property="email",
+ * type="string",
+ * format="email",
+ * description="Adresse email unique"
+ * ),
+ * @OA\Property(
+ * property="role",
+ * type="string",
+ * description="Rôle de l'utilisateur (ex: USER, ADMIN)",
+ * enum={"USER", "ADMIN"}
+ * ),
+ * @OA\Property(
+ * property="created_at",
+ * type="string",
+ * format="date-time",
+ * description="Date de création de l'utilisateur"
+ * ),
+ * @OA\Property(
+ * property="updated_at",
+ * type="string",
+ * format="date-time",
+ * description="Date de dernière mise à jour"
+ * )
+ * )
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -21,6 +62,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -41,5 +83,29 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'role' => 'string',
     ];
+
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Orders::class);
+    }
+
+    // public function notifications()
+    // {
+    //     return $this->hasMany(Notification::class);
+    // }
+
+    // Méthode utilitaire pour obtenir le panier actif
+    public function cart()
+    {
+        return $this->carts()->latest()->first();
+    }
 }
+
